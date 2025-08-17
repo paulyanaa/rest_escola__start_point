ec9b09c14f8b999d7c89f97e61868ec3ded56a6f
paulyanaa <136501842+paulyanaa@users.noreply.github.com>
2025-08-17 13:01:53 -0300

    Update cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ const { pool } = require('./db/pool');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
